e9ec04d2e2c2c37c2b980c5a0c87d70f1fb1315a
rommel dalisay <rommelmdalisay@gmail.com>
2024-01-24 17:02:44 +0800

    product card

--- Screens/Product/ProductContainer.js
+++ Screens/Product/ProductContainer.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { View, Text, StyleSheet, ActivityIndicator, FlatList } from 'react-native'
+import ProductList from "./ProductList";
 const data = require('../../assets/data/products.json')
+
 const ProductContainer = () => {
     const [products, setProducts] = useState([])
     useEffect(() => {
@@ -16,7 +18,7 @@ const ProductContainer = () => {
                 <FlatList
                     horizontal
                     data={products}
-                    renderItem={({ item }) => <Text>{item.brand}</Text>}
+                    renderItem={({ item }) => <ProductList key={item.id}/>}
                     keyExtractor={item => item.name}
                 />
             </View>
