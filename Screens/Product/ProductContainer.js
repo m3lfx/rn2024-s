9edb123718415b9dcb4cf8aa4912780921af9564
rommel dalisay <rommelmdalisay@gmail.com>
2024-01-24 17:22:02 +0800

    native base

--- Screens/Product/ProductContainer.js
+++ Screens/Product/ProductContainer.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, StyleSheet, ActivityIndicator, FlatList } from 'react-native'
+import { View, StyleSheet, ActivityIndicator, FlatList } from 'react-native'
+import { Container,  Icon, Item, Input, Text } from "native-base";
 import ProductList from "./ProductList";
 const data = require('../../assets/data/products.json')
 
@@ -12,19 +13,16 @@ const ProductContainer = () => {
         }
     }, [])
     return (
-        <View>
-            <Text>Product Container</Text>
             <View style={{ marginTop: 80 }} >
                 <FlatList
                     columnWrapperStyle={{ justifyContent: 'space-between' }}
                     numColumns={2}
                     data={products}
-                  
                     renderItem={({ item }) => <ProductList key={item.id} item={item} />}
                     keyExtractor={item => item.name}
                 />
             </View>
-        </View>
+        
     )
 }
 
