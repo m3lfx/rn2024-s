c0607863b0ba15ed0cddd5b0fd887a82bff9ac38
rommel dalisay <rommelmdalisay@gmail.com>
2024-01-27 08:22:44 +0800

    center component

--- Screens/Product/ProductContainer.js
+++ Screens/Product/ProductContainer.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { View, StyleSheet, ActivityIndicator, FlatList } from 'react-native'
-import { Container, Icon, Heading, Input, Text, NativeBaseProvider, extendTheme } from "native-base";
+import { Container, Icon, Heading, Input, Text, NativeBaseProvider, extendTheme, VStack, Center } from "native-base";
 import ProductList from "./ProductList";
 import { Ionicons } from "@expo/vector-icons";
 const data = require('../../assets/data/products.json')
@@ -22,12 +22,14 @@ const ProductContainer = () => {
         }
     }, [])
     return (
-        <NativeBaseProvider theme={theme}>
-            <Container>
-            <VStack w="100%" space={5} alignSelf="center">
-                <Heading fontSize="lg">Search</Heading>
-                <Input placeholder="Search" variant="filled" width="100%" borderRadius="10" py="1" px="2" InputLeftElement={<Icon ml="2" size="4" color="gray.400" as={<Ionicons name="search" />} />} />
-            </VStack>
+        <NativeBaseProvider
+            theme={theme}
+        >
+            <Center>
+                <VStack w="100%" space={5} alignSelf="center">
+                    <Heading fontSize="lg">Search</Heading>
+                    <Input placeholder="Search" variant="filled" width="100%" borderRadius="10" py="1" px="2" InputLeftElement={<Icon ml="2" size="4" color="gray.400" as={<Ionicons name="search" />} />} />
+                </VStack>
                 <View style={{ marginTop: 80 }} >
                     <FlatList
                         columnWrapperStyle={{ justifyContent: 'space-between' }}
@@ -37,7 +39,7 @@ const ProductContainer = () => {
                         keyExtractor={item => item.name}
                     />
                 </View>
-            </Container>
+            </Center>
         </NativeBaseProvider>
 
     )
