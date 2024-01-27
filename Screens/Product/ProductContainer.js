af72275c784069f8ebab8387c6bfe9f4c4681357
rommel dalisay <rommelmdalisay@gmail.com>
2024-01-27 08:38:04 +0800

    search product filter

--- Screens/Product/ProductContainer.js
+++ Screens/Product/ProductContainer.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
-import { View, StyleSheet, ActivityIndicator, FlatList } from 'react-native'
+import { View, StyleSheet, ActivityIndicator, FlatList, Dimensions } from 'react-native'
 import { Container, Icon, Heading, Input, Text, NativeBaseProvider, extendTheme, VStack, Center } from "native-base";
 import ProductList from "./ProductList";
 import { Ionicons } from "@expo/vector-icons";
+import SearchedProduct from "./SearchedProduct";
 const data = require('../../assets/data/products.json')
 
 const newColorTheme = {
@@ -13,14 +14,36 @@ const newColorTheme = {
     },
 };
 const theme = extendTheme({ colors: newColorTheme });
+var { height } = Dimensions.get('window')
 const ProductContainer = () => {
     const [products, setProducts] = useState([])
+    const [productsFiltered, setProductsFiltered] = useState([]);
+    const [focus, setFocus] = useState();
+    const searchProduct = (text) => {
+        setProductsFiltered(
+            products.filter((i) => i.name.toLowerCase().includes(text.toLowerCase()))
+        )
+    }
+    const openList = () => {
+        setFocus(true);
+    }
+
+    const onBlur = () => {
+        setFocus(false);
+    }
+
     useEffect(() => {
         setProducts(data);
+        setProductsFiltered(data);
+        setFocus(false)
         return () => {
             setProducts([])
+            setProductsFiltered([]);
+            setFocus()
         }
     }, [])
+
+
     return (
         <NativeBaseProvider
             theme={theme}
@@ -28,9 +51,39 @@ const ProductContainer = () => {
             <Center>
                 <VStack w="100%" space={5} alignSelf="center">
                     <Heading fontSize="lg">Search</Heading>
-                    <Input placeholder="Search" variant="filled" width="100%" borderRadius="10" py="1" px="2" InputLeftElement={<Icon ml="2" size="4" color="gray.400" as={<Ionicons name="search" />} />} />
+                    <Input placeholder="Search"
+                        onFocus={openList}
+                        onChangeText={(text) => searchProduct(text)}
+                        variant="filled"
+                        width="100%"
+                        borderRadius="10"
+                        py="1"
+                        px="2"
+                        InputLeftElement={<Icon ml="2" size="4" color="gray.400" as={<Ionicons name="search" />} />}
+                        InputRightElement={focus === true ? <Icon ml="2" size="4" color="gray.400" as={<Ionicons name="close" size="12" color="black" onPress={onBlur} />} /> : null}
+                    />
                 </VStack>
-                <View style={{ marginTop: 80 }} >
+                {focus === true ? (
+                    <SearchedProduct
+                        productsFiltered={productsFiltered}
+                    />
+                ) : (
+                    <View style={styles.container}>
+                        <Text>Product Container</Text>
+                        <View style={styles.listContainer} >
+                            <FlatList
+                                //    horizontal
+                                columnWrapperStyle={{ justifyContent: 'space-between' }}
+                                numColumns={2}
+                                data={products}
+
+                                renderItem={({ item }) => <ProductList key={item.brnad} item={item} />}
+                                keyExtractor={item => item.name}
+                            />
+                        </View>
+                    </View>
+                )}
+                {/* <View style={{ marginTop: 80 }} >
                     <FlatList
                         columnWrapperStyle={{ justifyContent: 'space-between' }}
                         numColumns={2}
@@ -38,11 +91,30 @@ const ProductContainer = () => {
                         renderItem={({ item }) => <ProductList key={item.id} item={item} />}
                         keyExtractor={item => item.name}
                     />
-                </View>
+                </View> */}
             </Center>
         </NativeBaseProvider>
 
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flexWrap: "wrap",
+        backgroundColor: "gainsboro",
+    },
+    listContainer: {
+        height: height,
+        flex: 1,
+        flexDirection: "row",
+        alignItems: "flex-start",
+        flexWrap: "wrap",
+        backgroundColor: "gainsboro",
+    },
+    center: {
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+});
 
 export default ProductContainer;
