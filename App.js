113a742f97a69526c0c31ce04879eed467db1569
rommel dalisay <rommelmdalisay@gmail.com>
2024-01-31 14:49:59 +0800

    cart actions

--- App.js
+++ App.js
@@ -2,12 +2,11 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 
-
-import ProductContainer from './Screens/Product/ProductContainer';
 import Header from './Shared/Header';
 import { NativeBaseProvider, extendTheme, } from "native-base";
 import { NavigationContainer } from '@react-navigation/native'
-import Main from './Navigators/Main'
+import Main from './Navigators/Main';
+
 import { Provider } from "react-redux";
 import store from "./Redux/store";
 
@@ -32,11 +31,4 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
