9bc230413707baaa6c732af8710a9c6f4305f62d
rommel dalisay <rommelmdalisay@gmail.com>
2024-02-07 16:08:18 +0800

    login error message

--- Screens/User/Login.js
+++ Screens/User/Login.js
@@ -1,50 +1,64 @@
 import Input from "../../Shared/Form/Input";
-import React, {useState} from "react";
-import {View, Text, StyleSheet } from 'react-native'
+import React, { useState } from "react";
+import { View, Text, StyleSheet } from 'react-native'
 import FormContainer from "../../Shared/Form/FormContainer";
-
+import Error from '../../Shared/Error'
 import { Button } from "native-base";
 
 const Login = (props) => {
 
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const [error, setError] = useState("")
+    const handleSubmit = () => {
+        const user = {
+            email,
+            password,
+        };
 
+        if (email === "" || password === "") {
+            setError("Please fill in your credentials");
+        } else {
+            //   loginUser(user, context.dispatch);
+            console.log("error")
+        }
+    }
     return (
-       <FormContainer>
-            <Input 
+        <FormContainer>
+            <Input
                 placeholder={"Enter email"}
                 name={"email"}
                 id={"email"}
                 value={email}
                 onChangeText={(text) => setEmail(text.toLowerCase())}
             />
-            <Input 
+            <Input
                 placeholder={"Enter Password"}
                 name={"password"}
                 id={"password"}
                 secureTextEntry={true}
                 value={password}
-                onChangeText={(text)=> setPassword(text)}
+                onChangeText={(text) => setPassword(text)}
             />
             <View style={styles.buttonGroup}>
-                <Button variant={"ghost"}>Login</Button>
+                {error ? <Error message={error} /> : null}
+                <Button variant={"ghost"} onPress={() => handleSubmit()}>Login</Button>
             </View>
-            <View style={[{marginTop:40} ,styles.buttonGroup]}>
+            <View style={[{ marginTop: 40 }, styles.buttonGroup]}>
                 <Text style={styles.middleText}>Dont' Have an Account yet?</Text>
-                <Button  variant={"ghost"} onPress={() => props.navigation.navigate("Register")} > Register</Button>
+                <Button variant={"ghost"} onPress={() => props.navigation.navigate("Register")} > Register</Button>
             </View>
-       </FormContainer>
+        </FormContainer>
     )
 }
 const styles = StyleSheet.create({
     buttonGroup: {
-      width: "80%",
-      alignItems: "center",
+        width: "80%",
+        alignItems: "center",
     },
     middleText: {
-      marginBottom: 20,
-      alignSelf: "center",
+        marginBottom: 20,
+        alignSelf: "center",
     },
-  });
+});
 export default Login
